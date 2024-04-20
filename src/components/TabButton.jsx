6c52578f516512e2bb51f36e3724cdc73ea68cf1
VanyaMark <markova.vanya@gmail.com>
2024-04-20 22:21:09 +0200

    first commit

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TabButton = ({ children, onClick, isSelected }) => {
+
+  return (
+    <li>
+      <button className={isSelected ? 'active' : ''} onClick={onClick} >{children}</button>
+    </li>
+  );
+};
+
+export default TabButton;
