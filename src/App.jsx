7c2c28c4e35941d7cf589b8a096bf290a2cd7b5a
VanyaMark <markova.vanya@gmail.com>
2024-04-20 22:57:31 +0200

    added Examples component and App refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,15 @@
-import { useState } from "react";
 import Header from "./components/Header/Header";
-import CoreConcept from "./components/CoreConcept/CoreConcept";
-import TabButton from "./components/TabButton";
 import CoreConcepts from "./components/CoreConcepts";
+import Examples from "./components/Examples";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("");
-
-  function handleClick(selectedButton) {
-    setSelectedTopic(selectedButton);
-    console.log(selectedButton);
-  }
 
   return (
     <div>
       <Header />
       <main>
         <CoreConcepts />
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleClick("components")}>
-              Components
-            </TabButton>
-            <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleClick("jsx")}>JSX</TabButton>
-            <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleClick("props")}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleClick("state")}>State</TabButton>
-          </menu>
-          {!selectedTopic ? <p>Please select a topic</p> : <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-              {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>}
-        </section>
+        <Examples />
       </main>
     </div>
   );
