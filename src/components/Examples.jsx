3275df512fd01604d2a17c839e460eac8b01a29f
VanyaMark <markova.vanya@gmail.com>
2024-04-20 23:30:38 +0200

    created Tabs component using pattern to add another slot in jsx component - buttons

---
+++ src/components/Examples.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { useState } from "react";
+import TabButton from "./TabButton";
+import Section from "./Section";
+import Tabs from "./Tabs";
+import { EXAMPLES } from "../data";
+
+const Examples = ({ handleClick }) => {
+  const [selectedTopic, setSelectedTopic] = useState("");
+
+  function handleClick(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedButton);
+  }
+  return (
+    <Section title="Examples" id="examples">
+      <Tabs
+        buttons={
+          <>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onClick={() => handleClick("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onClick={() => handleClick("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onClick={() => handleClick("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onClick={() => handleClick("state")}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
+        {!selectedTopic ? (
+          <p>Please select a topic</p>
+        ) : (
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+        )}
+      </Tabs>
+    </Section>
+  );
+};
+
+export default Examples;
